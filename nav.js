40ae0e9bcc93bf1ef3233afbc565535ed2e829aa
Ana Stuart <ana@anastuart.com>
2026-04-29 12:36:59 +1000

    interpersonal psychotherapy

--- nav.js
+++ nav.js
@@ -22,6 +22,7 @@ const NAV = [
       { id: 'five-ps',               title: 'The 5 Ps',              path: 'stems/five-ps.html' },
       { id: 'formulation-discussion', title: 'Presenting a Formulation', path: 'stems/formulation-discussion.html' },
       { id: 'motivational-interviewing', title: 'Motivational Interviewing', path: 'stems/motivational-interviewing.html' },
+      { id: 'interpersonal-psychotherapy', title: 'Interpersonal Psychotherapy', path: 'stems/interpersonal_psychotherapy.html' },
     ]
   },
   {
